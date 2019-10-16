5b54060b974f057bead8fa96ddeb20ca26262022
Tawhid <gem@tawhid.local>
2019-10-16 13:35:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Cat {
+    constructor(name){
+        this.name = name;
+    }
+
+    speak(){
+        return `i am ${this.name}`
+    }
+
+  }
+   
+
+
+  class Dog {
+    constructor(name){
+        this.name = name
+    }
+
+    speak(){
+        return `i am ${this.name}`
+    }
+
+  }
+   
+  class Bird {
+    constructor(name, gender){
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak(){
+        if(this.gender !== "male"){
+            return `squak`;
+        } else return `its me! ${this.name} the parrot`;
+        
+    }
+
+  }
+
+  let cat1 = new Cat("billy")
+  let dog1 = new Dog("woofwoof")
+  let bird1 = new Bird("timmy", "male")
+  let bird2 = new Bird("lana", "female")
